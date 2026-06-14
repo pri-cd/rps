faad0dbaf734ce5c52dc4320ef9b75c99bc2ffc1
Prithvee Raj Singh <prithvee11901@gmail.com>
2026-06-14 17:20:27 +0000

    Complete overhaul of UI, new logic and score card added

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,16 @@
-const MSG_HUMAN_WIN = "Human Wins";
-const MSG_MACHINE_WIN = "Machine Wins";
-const MSG_DRAW = "Draw";
+const HWIN_STATEMENT = "Human";
+const MWIN_STATEMENT = "Machine";
+const DRAW_STATEMENT = "Draw";
 const NO_OF_ROUNDS = 5;
+
+const HSCORE_CONTAINER = "humanScoreContainer";
+const MSCORE_CONTAINER = "machineScoreContainer";
 
 
 let humanScore = 0;
 let machineScore = 0;
 let numRounds = 0;
 
-
-function removeResultsIfExists() {
-    const resultMsgExists = document.querySelector("#showResults");
-    if (resultMsgExists) {
-        resultMsgExists.remove();
-    }
-}
 
 function resetScoreAndRound() {
     humanScore = 0;
@@ -35,7 +31,60 @@ function getMachineChoice() {
 }
 
 
+function displayResults(results) {
+
+    const humanInfo = document.querySelector("#humanInfoHolder");
+    const machineInfo = document.querySelector("#machineInfoHolder");
+
+    // Containers only defined so I can delete a complete containers while 
+    // cleaning results. (Only One Time Creation!)
+
+    if (!document.querySelector("#" + HSCORE_CONTAINER) && !document.querySelector("#" + MSCORE_CONTAINER)) {
+        const humanScoreContainer = document.createElement('div');
+        const machineScoreContainer = document.createElement('div');
+        machineScoreContainer.setAttribute('id', MSCORE_CONTAINER);
+        humanScoreContainer.setAttribute('id', HSCORE_CONTAINER);
+
+        humanInfo.appendChild(humanScoreContainer);
+        machineInfo.appendChild(machineScoreContainer);
+    }
+
+
+    const humanScoreBin = document.createElement("div");
+    const machineScoreBin = document.createElement("div");
+
+    humanScoreBin.classList.add("scoreText", "center");
+    machineScoreBin.classList.add("scoreText", "center");
+
+
+    if (results === HWIN_STATEMENT) {
+        humanScoreBin.textContent = "1";
+        machineScoreBin.textContent = "0";
+    }
+
+    if (results === DRAW_STATEMENT) {
+        humanScoreBin.textContent = "0";
+        machineScoreBin.textContent = "0"
+    }
+
+    if (results === MWIN_STATEMENT) {
+        humanScoreBin.textContent = "0";
+        machineScoreBin.textContent = "1";
+    }
+
+
+    humanScoreContainer.appendChild(humanScoreBin);
+    machineScoreContainer.appendChild(machineScoreBin);
+
+    if (numRounds == NO_OF_ROUNDS)
+    {
+        const resultMessage = document.querySelector("#resultMessage");
+        resultMessage.textContent = (humanScore > machineScore) ? "Human Wins 🔥" : ((machineScore > humanScore) ? "Machine Wins! 😹" : "Draw"); 
+    }
+}
+
 function executeLogicForGame(humanChoice, machineChoice) {
+    let results = DRAW_STATEMENT;
     const beats = {
         "rock": "scissors",
         "scissors": "paper",
@@ -43,42 +92,21 @@ function executeLogicForGame(humanChoice, machineChoice) {
     };
 
     if (humanChoice === machineChoice) {
-        return MSG_DRAW;
+        results = DRAW_STATEMENT;
     }
 
     if (beats[humanChoice] === machineChoice) {
         ++humanScore;
-        return MSG_HUMAN_WIN;
+        results = HWIN_STATEMENT;
     }
 
     if (beats[machineChoice] === humanChoice) {
         ++machineScore;
-        return MSG_MACHINE_WIN;
-    }
-}
-
-function displayResults() {
-    removeResultsIfExists();
-
-    const divResults = document.querySelector("#results");
-    const resultMsg = document.createElement('span');
-
-
-    resultMsg.setAttribute("id", "showResults");
-    resultMsg.style.color = "brown";
-    resultMsg.textContent = MSG_DRAW;
-
-    if (humanScore > machineScore) {
-        resultMsg.style.color = "green";
-        resultMsg.textContent = MSG_HUMAN_WIN;
+        results = MWIN_STATEMENT;
     }
 
-    if (machineScore > humanScore) {
-        resultMsg.style.color = "red";
-        resultMsg.textContent = MSG_MACHINE_WIN;
-    }
-
-    divResults.append(resultMsg);
+    displayResults(results);
+    return results;
 }
 
 
@@ -86,40 +114,64 @@ function playGame(humanChoice) {
     ++numRounds;
     let machineChoice = getMachineChoice();
     console.log(`> Results @ Round: ${numRounds} is > "${executeLogicForGame(humanChoice, machineChoice)}" (Human: ${humanChoice}, Machine: ${machineChoice})`);
-
-    if (numRounds === NO_OF_ROUNDS) {
-        displayResults();
-        resetScoreAndRound();
-
-    }
 }
 
-function addButtonListeners() {
+function addResetButtonListeners() {
 
+    const resetButton = document.querySelector("#resetGameButton");
+
+    resetButton.addEventListener('click', (event) => {
+        const humanScoreContainer = document.querySelector("#" + HSCORE_CONTAINER);
+        const machineScoreContainer = document.querySelector("#" + MSCORE_CONTAINER);
+        const resultMessage = document.querySelector("#resultMessage");
+
+        console.log(humanScoreContainer);
+        console.log(machineScoreContainer);
+
+        if (humanScoreContainer) {
+            humanScoreContainer.remove();
+        }
+
+        if (machineScoreContainer) {
+            machineScoreContainer.remove();
+        }
+        console.log("Resetting game");
+
+        resetScoreAndRound();
+        resultMessage.textContent = "";
+
+    });
+}
+
+function addGameButtonListeners() {
 
     const div = document.querySelector('#buttonContainer');
     div.addEventListener('click', (event) => {
-        let target = event.target;
+        if (numRounds < NO_OF_ROUNDS) {
 
-        if (event.target.tagName == "IMG") {
-            target = event.target.parentElement;
-        }
+            let target = event.target;
 
-        switch (target.id) {
-            case "r":
-                playGame("rock");
-                break;
-            case "p":
-                playGame("paper");
-                break;
-            case "s":
-                playGame("scissors");
-                break;
-            default:
-                console.log(event);
-                break;
+            if (event.target.tagName == "IMG") {
+                target = event.target.parentElement;
+            }
+
+            switch (target.id) {
+                case "r":
+                    playGame("rock");
+                    break;
+                case "p":
+                    playGame("paper");
+                    break;
+                case "s":
+                    playGame("scissors");
+                    break;
+                default:
+                    console.log(event);
+                    break;
+            }
         }
     });
 }
 
-addButtonListeners();
+addGameButtonListeners();
+addResetButtonListeners();
